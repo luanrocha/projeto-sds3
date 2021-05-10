1be5e24dad92b7781a8ca3a320edee16354e32b9
Luan Rocha de Souza <luanrocha@outlook.com>
2021-05-09 23:27:59 -0300

    Group by search

--- backend/src/main/java/com/luanrocha/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/luanrocha/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.luanrocha.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luanrocha.dsvendas.dto.SaleDTO;
+import com.luanrocha.dsvendas.dto.SaleSuccessDTO;
+import com.luanrocha.dsvendas.dto.SaleSumDTO;
 import com.luanrocha.dsvendas.entities.Sale;
 import com.luanrocha.dsvendas.repositories.SaleRepository;
 import com.luanrocha.dsvendas.repositories.SellerRepository;
@@ -24,5 +28,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
